ed08fa40cc56dcc1b6080f0c1e23b5fe03f6bd7d
nicky <nikkalaancing@gmail.com>
2023-06-02 19:37:19 +0700

    writing the logic of the cart

--- users/views.py
+++ users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.models import User
+
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -44,7 +45,7 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - профиль', 'form': form}
+    context = {'title': 'Store - профиль', 'form': form, 'baskets': Basket.objects.filter(user=request.user)}
     return render(request, 'users/profile.html', context)
 
 
