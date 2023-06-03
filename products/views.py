cd4ed0da3593c69171ffee765b6a089a7f988d4c
nicky <nikkalaancing@gmail.com>
2023-06-03 14:52:06 +0700

    implementation of pagination

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from products.models import ProductCategory, Product, Basket
-from users.models import User
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -9,16 +9,17 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)
-    else:
-        products = Product.objects.all()
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
 
     context = {
         'title': 'Store - Каталог',
         'categories': ProductCategory.objects.all(),
-        'products': products,
+        'products': products_paginator,
     }
     return render(request, 'products/products.html', context)
 
